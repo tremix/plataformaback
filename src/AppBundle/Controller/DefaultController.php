6315ff0ba8adbda27d85d04962ff7342186901c2
Tremix <npradomendo@gmail.com>
2017-11-04 17:06:50 +0000

    call json BD

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints as Assert; 
 use AppBundle\Services\Helpers;
 
 class DefaultController extends Controller
@@ -51,5 +52,53 @@ class DefaultController extends Controller
         
 
         
+    }
+
+    public function loginAction(Request $request)
+    {
+        /*echo "Hola Login";
+        die();*/
+
+        $helpers = $this->get(Helpers::class);
+
+        //Recibir Json por Post
+        $json = $request->get('json', null);
+
+        //array a devolver con data
+        $data = array(
+            'status' => 'error' ,
+            'data' => 'send json via post'
+         );
+
+        if ($json != null) {
+            //hacer Login
+            
+            //convertimos un json a un objeto de php
+            $params = json_decode($json);
+
+            $email = (isset($params->email)) ? $params->email : null;
+
+            $password = (isset($params->password)) ? $params->password : null;
+
+            $emailConstraint = new Assert\Email();
+            $emailConstraint->message = "This email is not valid!!";
+            $validate_email = $this->get("validator")->validate($email,$emailConstraint);
+
+            if($email != null && count($validate_email) == 0 && $password != null){
+
+                $data = array(
+                   'status' => 'success' ,
+                   'data' => 'OK'
+                );
+            }
+            else  { 
+            //     # code...
+                 $data = array(
+                   'status' => 'error' ,
+                   'data' => 'Email or Password incorrect'
+                 );
+            }
+        }
+        return $helpers->json($data);
     }
 }
