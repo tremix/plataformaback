40746d06ebb41a0c8c25bf9ee821f7bf554eae5e
Tremix <npradomendo@gmail.com>
2017-11-05 06:44:06 +0000

    Authorization token generation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Services\JwtAuth;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\Tests\Compiler\J;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Validator\Constraints as Assert; 
@@ -22,35 +23,48 @@ class DefaultController extends Controller
     }
 
     
-    public function pruebasAction(){
-
-        //echo "HOLA MUNDO NOPM";
-        $em =  $this->getDoctrine()->getManager();
-        $userRepo = $em->getRepository('BackendBundle:User');
-        $users = $userRepo->findAll();
-
-        /*var_dump($users[0]);
-        die();
-        */
-
-        /*return new JsonResponse(array(
-            'status' => 'success',
-            'users' => $users[0]->getName()
-        ));
-        */
-
+    public function pruebasAction(Request $request)
+    {
         $helpers = $this->get(Helpers::class);
-        /*echo $helpers->holaMundo();
-        die();*/
+        $jwt_auth = $this->get(JwtAuth::class);
+        $token = $request->get("authorization", null);
 
-        return $helpers->json($users);
-        die();
+        if ($token  && $jwt_auth->checkToken($token) == true) {
 
-        return $this->json(array(
-            'status' => 'success',
-            'users' => $users[0]->getName()
-        ));
-        
+            //echo "HOLA MUNDO NOPM";
+            $em = $this->getDoctrine()->getManager();
+            $userRepo = $em->getRepository('BackendBundle:User');
+            $users = $userRepo->findAll();
+
+            /*var_dump($users[0]);
+            die();
+            */
+
+            /*return new JsonResponse(array(
+                'status' => 'success',
+                'users' => $users[0]->getName()
+            ));
+            */
+
+
+            /*echo $helpers->holaMundo();
+            die();*/
+
+            //return $helpers->json($users);
+            //die();
+
+            return $this->json(array(
+                'status' => 'success',
+                'users' => $users[0]->getName()
+            ));
+        }else
+        {
+            return $this->json(array(
+                'status' => 'error',
+                'code' => 400,
+                'data' => 'Login Failed'
+            ));
+        }
 
         
     }
@@ -81,6 +95,8 @@ class DefaultController extends Controller
 
             $password = (isset($params->password)) ? $params->password : null;
 
+            $getHash = (isset($params->getHash)) ? $params->getHash : null;
+
             $emailConstraint = new Assert\Email();
             $emailConstraint->message = "This email is not valid!!";
             $validate_email = $this->get("validator")->validate($email,$emailConstraint);
@@ -88,13 +104,19 @@ class DefaultController extends Controller
             if($email != null && count($validate_email) == 0 && $password != null){
 
                 $jwt_auth = $this->get(JwtAuth::class);
-                $signup = $jwt_auth->signup($email,$password);
 
-                $data = array(
+                if ($getHash == null || $getHash == false){
+                    $signup = $jwt_auth->signup($email,$password);
+                }else{
+                    $signup = $jwt_auth->signup($email,$password, true);
+                }
+
+                return $this->json($signup);
+                /*$data = array(
                    'status' => 'success' ,
                    'data' => 'OK',
                     'signup' => $signup
-                );
+                );*/
             }
             else  { 
             //     # code...
--- src/AppBundle/Services/JwtAuth.php
+++ src/AppBundle/Services/JwtAuth.php
@@ -7,13 +7,15 @@ use Firebase\JWT\JWT;
 class JwtAuth
 {
     public $manager;
+    public $key;
 
     public function __construct($manager)
     {
         $this->manager = $manager;
+        $this->key = 'holaquetalsoylaclave';
     }
 
-    public function signup($email, $password)
+    public function signup($email, $password, $getHash = null)
     {
         $user = $this->manager->getRepository('BackendBundle:User')->findOneBy(array(
             'email' => $email,
@@ -28,10 +30,30 @@ class JwtAuth
         if ($sigunp == true) {
             //Generar Token
 
-            $data = array(
+            $token =array(
+                "sub" => $user->getId(),
+                "email" => $user->getEmail(),
+                "name" => $user->getName(),
+                "surname" => $user->getSurname(),
+                "iat" => time(),
+                "exp" => time() + (7*24*60*60)
+            );
+
+            $jwt = JWT::encode($token, $this->key, 'HS256');
+            $decoded = JWT::decode($jwt,$this->key, array('HS256'));
+
+            if($getHash == null){
+                $data = $jwt;
+            }else{
+                $data = $decoded;
+            }
+
+            $data = $jwt;
+
+            /*$data = array(
                 'status' => 'success',
                 'user' => $user
-            );
+            );*/
         } else {
             $data = array(
                 'status' => 'error',
@@ -41,5 +63,33 @@ class JwtAuth
 
         //return $email. " ".$password;
         return $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function checkToken($jwt, $getIdentity = false)
+    {
+        $auth =false;
+
+        try {
+            $decoded = JWT::decode($jwt, $this->key, array('HS256'));
+        } catch (\UnexpectedValueException $e) {
+            $auth = false;
+        } catch (\DomainException $e){
+            $auth = false;
+        }
+
+        if (isset($decoded) && is_object($decoded) && isset($decoded->sub)) {
+            $auth = true;
+        } else {
+            $auth = false;
+        }
+
+        if ($getIdentity == false) {
+            return $auth;
+        } else {
+            return $decoded;
+        }
     }
 }
