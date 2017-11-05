de3c89bf673e521832ad687a765847c1e019df56
Tremix <npradomendo@gmail.com>
2017-11-05 13:09:58 +0000

    User Edit

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints as Assert;
 use BackendBundle\Entity\User;
 use AppBundle\Services\Helpers;
+use AppBundle\Services\JwtAuth;
 
 class UserController extends Controller{
 
@@ -76,4 +77,98 @@ class UserController extends Controller{
         return $helpers->json($data);
     }
 
+
+    public function editAction(Request $request)
+    {
+        $helpers = $this->get(Helpers::class);
+        $jwt_auth = $this->get(JwtAuth::class);
+
+        $token = $request->get('authorization', null);
+        $authCheck = $jwt_auth->checkToken($token);
+
+        if($authCheck) {
+            //Entity manager
+            $em = $this->getDoctrine()->getManager();
+
+            // Conseguir los datos del usuario indentificado via token
+            $identity = $jwt_auth->checkToken($token, true);
+
+            // Conseguir el objeto a actualizar
+            $user = $em->getRepository('BackendBundle:User')->findOneBy(array(
+                'id' => $identity->sub
+            ));
+
+            //recoger los datos por post
+            $json = $request->get('json', null);
+            $params = json_decode($json);
+
+            //array de error por defecto
+
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'msg' => 'User not updated'
+            );
+
+
+            if ($json != null) {
+                //$createdAt = new \DateTime("now");
+                $role = 'user';
+
+                $email = (isset($params->email)) ? $params->email : null;
+                $name = (isset($params->name)) ? $params->name : null;
+                $surname = (isset($params->surname)) ? $params->surname : null;
+                $password = (isset($params->password)) ? $params->password : null;
+
+                $emailConstraint = new Assert\Email();
+                $emailConstraint->message = "This email is not valid";
+                $validate_email = $this->get("validator")->validate($email, $emailConstraint);
+
+                if ($email != null && count($validate_email) == 0 && $password != null && $name != null && $surname != null) {
+
+                    //$user->setCreatedAt($createdAt);
+                    $user->setRole($role);
+                    $user->setEmail($email);
+                    $user->setName($name);
+                    $user->setSurname($surname);
+
+
+                    $isset_user = $em->getRepository('BackendBundle:User')->findBy(array(
+                        'email' => $email
+                    ));
+
+                    if (count($isset_user) == 0 || $identity->email == $email) {
+                        $em->persist($user);
+                        $em->flush();
+
+                        $data = array(
+                            'status' => 'success',
+                            'code' => 200,
+                            'msg' => 'Updated User updated!!',
+                            'user' => $user
+                        );
+                    } else {
+
+                        $data = array(
+                            'status' => 'error',
+                            'code' => 400,
+                            'msg' => 'User not updated, duplicated!!'
+                        );
+                    }
+                }
+
+
+            } else {
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'msg' => 'Authorization not valid!!'
+                );
+
+            }
+        }
+
+        return $helpers->json($data);
+
+    }
 }
